ac5bf375b40076401cfeb203b7cf78482ceb6581
Basil Ralph Birondo <basilralphbirondo@outlook.com>
2025-11-11 22:49:05 +0800

    Clear old files

--- UserSession.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EasyEv3
-{
-    public static class UserSession
-    {
-        public static int UserId { get; set; }
-        public static string Username { get; set; }
-        public static int Role { get; set; }
-    }
-}
